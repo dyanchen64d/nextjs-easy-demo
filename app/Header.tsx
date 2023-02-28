0eed5884230e87f49048e54d670c080b9509c6c5
chen.zhang <zhangchen64d@gmail.com>
2023-02-28 22:20:12 +0900

    update

---
+++ app/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className=" p-5 bg-blue-500">
+      <p className=" font-bold text-white">
+        <Link href="/" className=" px-2 py-1 bg-white text-blue-500 rounded-lg">
+          Home
+        </Link>
+      </p>
+    </header>
+  );
+}
---
+++ app/todos/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodosList from './TodosList';
+
+export default function Todos() {
+  return (
+    <div>
+      {/* @ts-ignore */}
+      <TodosList />
+    </div>
+  );
+}
